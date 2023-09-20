0dcb95404d62622f83b2c063c55dc62b901303ac
Johnson Absolu <Johnson.absolu@outlook.com>
2023-09-19 22:33:37 -0400

    Updated about text

--- src/assets.js
+++ src/assets.js
@@ -12,23 +12,21 @@ export const user = {
 
 export const WebsiteTexts = {
     title : `I'm a Software Engineer`,
-    subtitle: `Based in Connecticut, I'm a Software 
-    Engineer passionate about building accessible web apps that users love.`,
+    subtitle: `Based in Connecticut, I'm a Software Engineer passionate about building accessible web apps that users love.`,
     about: `I'm Johnson Absolu, a passionate Front-end software 
-    engineer specializing in building responsive web applications. 
-    Currently enrolled in a bachelor's degree program in computer 
-    science at Central Connecticut State University, my journey 
-    in front-end development began in 2018 when I successfully 
-    launched an ecommerce website on Shopify. Since then, I have 
-    honed my skills in building ecommerce sites on Shopify and 
-    ventured into freelancing, helping individuals launch their 
-    online stores. In 2020, I embarked on a learning path with 
-    FreeCode Camp, where I acquired the knowledge to create basic web apps.`,
-
+            engineer specializing in building responsive web applications. 
+            Currently enrolled in a bachelor's degree program in computer 
+            science at Southern Connecticut State University, my journey 
+            in front-end development began in 2018 when I successfully 
+            launched an ecommerce website on Shopify. Since then, I have 
+            honed my skills in building ecommerce sites on Shopify and 
+            ventured into freelancing, helping individuals launch their 
+            online stores. In 2020, I enrolled on a free online bootcamp on
+            FreeCodeCamp, where I acquired the knowledge to create basic websites using HTML, CSS and Javascript.`,
     Status_searching: `I am actively looking for an Entry-level or Apprenticeship opportunity, 
-    Please do not hesitate to email me using the link below regarding potential positions.`,
-    Status_not_searching: `Hi, thanks for visiting my portfolio website. 
-    Feel free to send me an email.`
+            Please do not hesitate to email me using the link below regarding potential positions.`,
+            Status_not_searching: `Hi, thanks for visiting my portfolio website. 
+            Feel free to send me an email.`
 }
 
 
