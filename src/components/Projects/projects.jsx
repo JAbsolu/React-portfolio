7cfd3a484f27618cd74dc614c1903f2121001e72
John Absolu <Johnson.absolu@hotmail.com>
2025-07-21 19:16:41 -0400

    Added Notesage project, and removed other projects

--- src/components/Projects/projects.jsx
+++ src/components/Projects/projects.jsx
@@ -5,45 +5,53 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import FlexBetween from "../flexBetween";
 import { themeSettings } from "../../theme";
-// import { useMediaQuery } from "@mui/material";
 import { projects } from "../../resources";
 
 const ProjectCards = () => {
-  // const isMac = useMediaQuery("(min-width: 1400px)");
-
   const green = themeSettings.palette.primary.green;
   const navy = themeSettings.palette.primary.navy;
   const lightSlate = themeSettings.palette.primary.lightSlate;
   
   return (
     <Box sx={{ display: "flex", gap: "1rem", }}>
-    {projects.length > 0  && projects.map((project) => {
-      const name = project.name;
-      const frontend = project.frontendView;
-      const github = project.github;
-      const technologies = project.technology;
-      const description = project.description;
+      {projects.length > 0  && projects.map((project) => {
+        const name = project.name;
+        const frontend = project.frontendView;
+        const github = project.github;
+        const technologies = project.technology;
+        const description = project.description;
 
-      return (
-         <Link href={frontend} underline="none" target="_new">
-        <Card
-          sx={{
-            minWidth: "28rem",
-            minHeight: "16rem",
-            maxHeight: "17rem",
-            background: navy,
-            borderRadius: "5px",
-            mt: "5px",
-          }}
-        >
-          <CardContent
+        return (
+          <Link href={frontend} underline="none" target="_new">
+          <Card
             sx={{
-              p: "2rem",
+              minWidth: "28rem",
+              minHeight: "16rem",
+              maxHeight: "17rem",
+              background: navy,
+              borderRadius: "5px",
+              mt: "5px",
             }}
           >
-            <FlexBetween>
-              <Link href={github} target="_new">
-                <GitHubIcon
+            <CardContent
+              sx={{
+                p: "2rem",
+              }}
+            >
+              <FlexBetween>
+                <Link href={github} target="_new">
+                  <GitHubIcon
+                    sx={{
+                      color: lightSlate,
+                      fontSize: "2rem",
+                      "&:hover": {
+                        color: green,
+                        cursor: "pointer",
+                      },
+                    }}
+                  />
+                </Link>
+                <LaunchIcon
                   sx={{
                     color: lightSlate,
                     fontSize: "2rem",
@@ -53,65 +61,49 @@ const ProjectCards = () => {
                     },
                   }}
                 />
-              </Link>
-              <LaunchIcon
+              </FlexBetween>
+
+              <Typography
+                variant="h7"
+                sx={{
+                  color: green,
+                  pt: "2rem",
+                  pd: "0.5rem",
+                }}
+              >
+                {name}
+              </Typography>
+
+              <Typography
                 sx={{
                   color: lightSlate,
-                  fontSize: "2rem",
-                  "&:hover": {
-                    color: green,
-                    cursor: "pointer",
-                  },
+                  lineHeight: "1.5",
+                  fontSize: "0.9rem",
+                  fontWeight: "100",
+                  pt: "0.5rem",
+                  pb: "2rem",
                 }}
-              />
-            </FlexBetween>
+              >
+                {description}
+              </Typography>
 
-            <Typography
-              variant="h7"
-              sx={{
-                color: green,
-                pt: "2rem",
-                pd: "0.5rem",
-              }}
-            >
-              {name}
-            </Typography>
-
-            <Typography
-              sx={{
-                color: lightSlate,
-                lineHeight: "1.5",
-                fontSize: "0.9rem",
-                fontWeight: "100",
-                pt: "0.5rem",
-                pb: "2rem",
-              }}
-            >
-              {description}
-            </Typography>
-
-            <FlexBetween
-              sx={{
-                pb: "0",
-              }}
-            >
-              {technologies.map((value) => (
-                <Typography
-                  sx={{
-                    color: lightSlate,
-                    fontWeight: "200",
-                    fontSize: "0.85rem",
-                  }}
-                >
-                  {value}
-                </Typography>
-              ))}
-            </FlexBetween>
-          </CardContent>
-        </Card>
-      </Link>
-      )
-    })}
+              <FlexBetween sx={{ pb: "0" }} >
+                {technologies.map((value) => (
+                  <Typography key={value} sx={{
+                      color: lightSlate,
+                      fontWeight: "200",
+                      fontSize: "0.85rem",
+                    }}
+                  >
+                    {value}
+                  </Typography>
+                ))}
+              </FlexBetween>
+            </CardContent>
+          </Card>
+        </Link>
+        )
+      })}
     </Box>
   )
 };
--- src/resources.js
+++ src/resources.js
@@ -34,12 +34,45 @@ export const projects = [
     description: "An AI app that leverages openAI-GPT-40 and Yahoo Finance to determine whether a stock is a bearish or bullish.",
   },
   {
+    name: "Notesage - Quiz and Notes App",
+    frontendView: 'https://www.note-sage.com/',
+    github: "https://github.com/JAbsolu/Invest.ai",
+    technology: ["Node JS", "Express JS", "Mongo DB", "AWS", "Next JS", "Material UI", "Tailwind CSS", "Javascript"],
+    description: `NoteSage is a full-stack web application that helps students create and study from custom flashcards 
+                  and quizzes based on their own notes. It simplifies the study process by making it easy to turn class 
+                  materials into personalized flashcards and quizzes. By focusing on user-created content, NoteSage 
+                  promotes active learning and better retention.`,
+  },
+  {
+      name: "React Todo List Application",
+      frontendView: "https://todo-blond-theta.vercel.app/",
+      github: "https://github.com/JAbsolu/Todo-app",
+      technology: ["Javascript", "React Js", "CSS"],
+      description:
+        "A single page application that allows users to add tasks to a list to be completed.",
+    },
+  {
+      name: "Weather App",
+      frontendView: "https://htweather.vercel.app/",
+      github: "https://github.com/JAbsolu/weather-app",
+      technology: ["React", "Weather API", "Geolocation API" , "Material UI"],
+      description: "A weather application with weather and geolocation API, allowing users to get weather info based on their live location.",
+    },
+  {
     name: "Event Management App",
     // frontendView: 'https://ctstategpa.vercel.app/',
     github: "https://github.com/JAbsolu/Invest.ai",
     technology: ["Node JS", "MYSQL", "Google Cloud"],
     description: "A Fullstack Application that allows students, staff, and faculty to create, and manage events.",
   },
+   {
+      name: "Social Media App",
+      frontendView: null,
+      github: "https://github.com/JAbsolu/FullStack-Social-app",
+      technology: ["React", "Redux", "Node JS", "MongoDb", "Express JS"],
+      description:
+        "A Fullstack social media website, allows users to signup/signin and add friends.",
+    },
   {
     name: "CT State GPA Calculator",
     frontendView: 'https://ctstategpa.vercel.app/',
@@ -48,57 +81,19 @@ export const projects = [
     description: "A GPA calulator intended to be used for the Connecticut State Colleges.",
   },
   {
+      name: "Full-Stack Dealership Application",
+      frontendView: null,
+      github: "https://github.com/JAbsolu/Serverside-programing-Final-Project",
+      technology: ["PHP", "MySQL", "Bootstrap", "HTML", "CSS", "JavaScript"],
+      description: "A Full-stack car dealership app with a MySQL database",
+    },
+  {
     name: "Team and Task Manager App",
     frontendView: null,
     github: "https://github.com/JAbsolu/Team-management-app",
     technology: ["React JS", "Node JS", "Mongodb", "Express JS" , "MUI"],
     description: "A MERN Stack team and task manager app, that allows teams to colloborate, assigns tasks based on their respective levels.",
   },
-    {
-      name: "Weather App",
-      frontendView: "https://htweather.vercel.app/",
-      github: "https://github.com/JAbsolu/weather-app",
-      technology: ["React", "Weather API", "Geolocation API" , "Material UI"],
-      description: "A weather application with weather and geolocation API, allowing users to get weather info based on their live location.",
-    },
-    {
-      name: "Full-Stack Dealership Application",
-      frontendView: null,
-      github: "https://github.com/JAbsolu/Serverside-programing-Final-Project",
-      technology: ["PHP", "MySQL", "Bootstrap", "Hack"],
-      description: "A Full-stack car dealership app with a MySQL database",
-    },
-    {
-      name: "React Todo List Application",
-      frontendView: "https://todo-blond-theta.vercel.app/",
-      github: "https://github.com/JAbsolu/Todo-app",
-      technology: ["Javascript", "React Js", "CSS"],
-      description:
-        "A single page application that allows users to add tasks to a list to be completed.",
-    },
-    {
-      name: "MERN Social Media App",
-      frontendView: null,
-      github: "https://github.com/JAbsolu/FullStack-Social-app",
-      technology: ["React", "Redux", "Node JS", "MongoDb"],
-      description:
-        "A Fullstack social media website, allows users to signup/signin and add friends.",
-    },
-    {
-      name: "Javascript calculator",
-      frontendView: "https://jscalculator-ruby.vercel.app/",
-      github: "https://github.com/JAbsolu/Javascript-calculator",
-      technology: ["HTML", "CSS", "Javascript"],
-      description: "A javascript calculator that allows users to calculate",
-    },
-    {
-      name: "Emergency Response App",
-      frontendView: "https://emergency-response-app.vercel.app/",
-      github: "https://github.com/JAbsolu/Emergency-response-app",
-      technology: ["Javascript", "React Js", "Sass", "CSS", "Bootstrap"],
-      description:
-        "A 4 page web application that allows users to login and request emergency assistance.",
-    },
     {
       name: "Ecommerce Website",
       frontendView: "https://sneakeryoo.vercel.app",
@@ -108,17 +103,11 @@ export const projects = [
         "A multi-page ecommerce website that allows users to view a product and add to cart.",
     },
     {
-      name: "Lease Payment Calculator",
-      frontendView: null,
-      github: "https://github.com/JAbsolu/lease-payment-calculator",
-      technology: ["PHP", "HTML", "Bootstrap", "CSS"],
-      description: "A one page web app that helps calculate car lease payment. Used in the Fullstack dealership project.",
-    },
-    {
-      name: "Leetcode Solutions",
-      frontendView: "https://github.com/JAbsolu/Leetcode-Solutions",
-      github: "https://github.com/JAbsolu/Leetcode-Solutions",
-      technology: ["Javascript", "Python"],
-      description: "A list of my leetcode solutions. New solutions are added weekly",
+      name: "Emergency Response App",
+      frontendView: "https://emergency-response-app.vercel.app/",
+      github: "https://github.com/JAbsolu/Emergency-response-app",
+      technology: ["Javascript", "React Js", "Sass", "CSS", "Bootstrap"],
+      description:
+        "A 4 page web application that allows users to login and request emergency assistance.",
     },
   ];
